62881e0df839d9ea216c992def9c27393a7f6cb7
Daniel Tieber <daniel.tieber@redbull.com>
2018-09-30 14:13:04 +0200

    solution

--- test/contracts/SassyProviderTest.scala
+++ test/contracts/SassyProviderTest.scala
@@ -1,8 +1,18 @@
 package contracts
 
-import org.scalatest.{AsyncFunSpec, Matchers}
+import com.itv.scalapact.ScalaPactVerify.{loadFromLocal, verifyPact}
+import org.scalatest.{FunSpec, Matchers}
+import com.itv.scalapact.circe09._
+import com.itv.scalapact.http4s18._
 
-class SassyProviderTest extends AsyncFunSpec with Matchers {
+class SassyProviderTest extends FunSpec with Matchers {
 
-
+  describe("ArtistService") {
+    it("should return list of artist") {
+      verifyPact
+        .withPactSource(loadFromLocal("./"))
+        .noSetupRequired
+        .runVerificationAgainst("http", "localhost", 9000)
+    }
+  }
 }

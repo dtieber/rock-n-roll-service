f663647978b8cd6fe087e7b03437f628bc9e2897
Daniel Tieber <daniel.tieber@redbull.com>
2018-10-01 20:59:56 +0200

    add implicits

--- test/contracts/SassyProviderTest.scala
+++ test/contracts/SassyProviderTest.scala
@@ -1,6 +1,8 @@
 package contracts
 
 import org.scalatest.{AsyncFunSpec, Matchers}
+import com.itv.scalapact.circe09._
+import com.itv.scalapact.http4s18._
 
 class SassyProviderTest extends AsyncFunSpec with Matchers {
 
